25842a53d76ca17a8fa80c44c6d3063f19680c4c
0xWerz <moussarabie40@gmail.com>
2023-03-15 00:34:50 +0100

    class to xpath

--- js/content.js
+++ js/content.js
@@ -1,12 +1,18 @@
-function checkAndEditDOM(className, newContent) {
-    var elements = document.getElementsByClassName(className);
-    if (elements.length > 0) {
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].innerHTML = newContent;
+function checkAndEditDOM(xpath, newContent) {
+    var elements = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    );
+    if (elements.snapshotLength > 0) {
+        for (var i = 0; i < elements.snapshotLength; i++) {
+            elements.snapshotItem(i).innerHTML = newContent;
         }
-        return "Class name found and edited successfully!";
+        return "XPath found and edited successfully!";
     } else {
-        return "Class name not found on the current page.";
+        return "XPath not found on the current page.";
     }
 }
 
@@ -14,12 +20,11 @@ function refreshPage() {
     location.reload();
 }
 
-chrome.storage.sync.get(["className", "contentName"], function (data) {
-    var className = data.className;
+chrome.storage.sync.get(["xpath", "contentName"], function (data) {
+    var xpath = data.xpath;
     var newContent = data.contentName;
-    if (className && newContent) {
-        var response = checkAndEditDOM(className, newContent);
+    if (xpath && newContent) {
+        var response = checkAndEditDOM(xpath, newContent);
         document.getElementById("status").innerHTML = response;
     }
 });
-
--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,18 @@
 document.getElementById("save-button").addEventListener("click", function () {
-  var className = document.getElementById("class-name").value;
-  var contentName = document.getElementById("content-name").value;
-  chrome.storage.sync.set({ className: className, contentName: contentName }, function () {
-    document.getElementById("status").innerHTML = "Changes saved. Please refresh the page to see the changes.";
-  });
+    var xpath = document.getElementById("xpath").value;
+    var contentName = document.getElementById("content-name").value;
+    chrome.storage.sync.set(
+        { xpath: xpath, contentName: contentName },
+        function () {
+            document.getElementById("status").innerHTML =
+                "Changes saved. Please refresh the page to see the changes.";
+        }
+    );
 });
 
 document.getElementById("reset-button").addEventListener("click", function () {
-  chrome.storage.sync.clear(function () {
-    document.getElementById("status").innerHTML = "Changes reset. Please refresh the page to see the changes.";
-  });
+    chrome.storage.sync.clear(function () {
+        document.getElementById("status").innerHTML =
+            "Changes reset. Please refresh the page to see the changes.";
+    });
 });
